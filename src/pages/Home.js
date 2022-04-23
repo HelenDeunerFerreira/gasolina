0f4c1ea179fc30c0d5dcae24bcfeeecde99e8bf3
HelenDeunerFerreira <helendeunerferreira@gmail.com>
2022-04-23 16:31:02 -0300

    Adição do async storage e ajustes finais

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,38 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Home({ navigation }) {
-    const [kilometros, setKilometros] = useState(' ');
-    const [litros, setLitros] = useState(' ');
+    const [kilometros, setKilometros] = useState('');
+    const [litros, setLitros] = useState('');
 
-    const verificacao = () => {
-        const kmRodados = parseFloat(kilometros)
-        const litrosConsumidos = parseFloat(litros)
+    const verificacao = async () => {
+        function convert_to_float(str) {
+            var floatValue = +(str);
+            return floatValue;
+        }
 
-        const calculo = kmRodados / litrosConsumidos
+        let kmRodados = convert_to_float(kilometros)
+        let gasolinaConsumida = convert_to_float(litros)
+
+        setKilometros(kmRodados)
+        setLitros(gasolinaConsumida)
+
+        const calculo = kmRodados / gasolinaConsumida
 
         if (calculo != String) {
             navigation.navigate('Resultado')
         } else {
             console.log('Dados inseridos inválidos, tente novamente')
         }
+
+        const calculoString = String(calculo)
+
+        await AsyncStorage.setItem(
+            "@Home",
+            calculoString
+        );
     }
 
     return (
@@ -25,20 +41,22 @@ export default function Home({ navigation }) {
                 Escreva as informações para calcular o consumo de gasolina do seu carro
             </Text>
 
+            <Text style={styles.texto}>Quilômetros rodados</Text>
             <TextInput
+                placeholder="km"
                 style={styles.input}
                 keyboardType="numeric"
-                placeholder="Quilometragem"
-                onChangeText={(kilometros) => setKilometros(kilometros)}
-                defaultValue={kilometros}
+                onChangeText={(kilometros) => setKilometros(String(kilometros))}
+                defaultValue={String(kilometros)}
             />
 
+            <Text style={styles.texto}>Gasolina consumida</Text>
             <TextInput
+                placeholder="L"
                 style={styles.input}
                 keyboardType="numeric"
-                placeholder="Gasolina consumida"
-                onChangeText={(litros) => setLitros(litros)}
-                defaultValue={litros}
+                onChangeText={(litros) => setLitros(String(litros))}
+                defaultValue={String(litros)}
             />
 
             <Button
@@ -55,6 +73,7 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
@@ -62,18 +81,23 @@ const styles = StyleSheet.create({
     input: {
         height: 50,
         width: 300,
+
+        fontSize: 18,
         alignItems: "center",
         justifyContent: "center",
         borderColor: "#000",
+
         borderRadius: 15,
         borderWidth: 2,
         margin: 15,
         padding: 10,
-        fontSize: 18,
     },
     titulo: {
         fontSize: 25,
         padding: 10,
         margin: 5
+    },
+    texto: {
+        fontSize: 17
     }
 });
--- src/pages/Resultado.js
+++ src/pages/Resultado.js
@@ -1,31 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Resultado() {
     const [resultado, setResultado] = useState(" ");
     const [indicacao, setIndicacao] = useState(" ");
 
-    const calculo = 13
-
     useEffect(() => {
-        setResultado(calculo)
 
-        const indicacaoDeConsumo = () => {
+        const indicacaoDeConsumo = async () => {
+
+            const resultadoCalculado = await AsyncStorage.getItem(
+                "@Home"
+            );
+
+            function convert_to_float(str) {
+                var floatValue = +(str);
+                return floatValue;
+            }
+
+            let conta = convert_to_float(resultadoCalculado)
+
+            setResultado(conta)
+
             if (resultado > 12) {
                 setIndicacao('A')
-            } else if (resultado < 12 && resultado > 10) {
+            } else if (resultado <= 12 && resultado > 10) {
                 setIndicacao('B')
-            } else if (resultado < 10 && resultado > 8) {
+            } else if (resultado <= 10 && resultado > 8) {
                 setIndicacao('C')
-            } else if (resultado < 8 && resultado > 4) {
+            } else if (resultado <= 8 && resultado > 4) {
                 setIndicacao('D')
-            } else {
+            } else if (resultado <= 4) {
                 setIndicacao('E')
             }
         }
 
         indicacaoDeConsumo()
+
     }, []);
 
     return (
@@ -40,12 +53,14 @@ export default function Resultado() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
     titulo: {
         fontSize: 20,
+
         padding: 10,
         margin: 5
     }
